36e2289609985f2a9c60cde0d70f86934cf79bad
Evelina Sokolovskaya <sokolovskayaevi@gmail.com>
2021-04-06 21:03:34 +0300

    create Button and Header Components

---
+++ src/components/Button.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import classNames from 'classnames'
+
+type PropsType = {
+    className?: string
+    outline?: boolean
+    onClick?: () => void
+}
+
+const Button: React.FC<PropsType> = ({className, outline, children, onClick}) => {
+    return (
+        <button className={classNames('button', className,
+            {
+                'button--outline': outline
+            }
+        )}>
+            {children}
+        </button>
+    );
+};
+
+export default Button;
